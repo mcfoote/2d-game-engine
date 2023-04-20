34ba6531750dc0021c0822ed50795d94e1cffa82
Mitchell Foote <mcfoote321@gmail.com>
2023-04-19 22:06:17 -0400

    work on game loop

--- SDL2-engine/SDL2-engine/main.cpp
+++ SDL2-engine/SDL2-engine/main.cpp
@@ -2,6 +2,7 @@
 
 int main(int argc, char* argv[]) {
 
+	/*
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window *window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
@@ -13,6 +14,7 @@ int main(int argc, char* argv[]) {
 	SDL_RenderPresent(renderer);
 
 	SDL_Delay(3000);
+	*/
 
 	return 0;
 
