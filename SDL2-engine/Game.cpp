82d133a104e024387dc4a8536d52258f050bb05e
Mitchell Foote <mcfoote321@gmail.com>
2023-04-28 20:47:15 -0400

    add fps limiter

---
+++ SDL2-engine/Game.cpp
@@ -0,0 +1,87 @@
+#include "Game.h"
+
+SDL_Texture* playerTexture;
+SDL_Rect srcR, destR;
+
+Game::Game() {
+}
+
+Game::~Game() {
+
+}
+
+void Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen) {
+
+	int flags = 0;
+	if (fullscreen) {
+		flags = SDL_WINDOW_FULLSCREEN;
+	}
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+		std::cout << "Subsystems Successfully Initialised..." << std::endl;
+
+		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+		if (window) {
+			std::cout << "Window Successfully Created..." << std::endl;
+		}
+
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer) {
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+			std::cout << "Renderer Successfully Created..." << std::endl;
+		}
+
+		isRunning = true;
+	} else {
+		isRunning = false;
+	}
+
+	SDL_Surface* tmpSurface = IMG_Load("assets/playertest.png");
+	playerTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	SDL_FreeSurface(tmpSurface);
+
+}
+
+void Game::handleEvents() {
+
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type) {
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+		default:
+			break;
+	}
+
+}
+
+void Game::update() {
+
+	count++;
+
+	destR.h = 64;
+	destR.w = 64;
+	destR.x = count;
+
+	std::cout << count << std::endl;
+
+}
+
+void Game::render() {
+
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, playerTexture, NULL, &destR);
+	SDL_RenderPresent(renderer);
+
+}
+
+void Game::clean() {
+
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+
+	std::cout << "Game Resources Cleaned Up" << std::endl;
+
+}
